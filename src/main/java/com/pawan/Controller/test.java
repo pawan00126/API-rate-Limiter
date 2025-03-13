22328214f16718cbd24f844a7f43f59ab7ed7268
pawan00126 <pawanbhati2002@gmail.com>
2025-03-13 17:35:20 +0530

    updated the test class

--- src/main/java/com/pawan/Controller/test.java
+++ src/main/java/com/pawan/Controller/test.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class test {
 
     @GetMapping
-    public String test(){
+    public String Home(){
         return "Hello, User.....";
     }
 }
